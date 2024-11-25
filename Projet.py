34126f47ba82df1625eee9fbbd2d279593147716
Mathisrrr <mathisrivallan5@gmail.com>
2024-11-25 11:30:29 +0100

    new

--- Projet.py
+++ Projet.py
@@ -2,6 +2,7 @@ from tkiteasy import *
 import numpy as np
 X,Y=800,800
 g=ouvrirFenetre(X,Y)
+
 #Dictionnaire qui renvoie la coordonnée de la grille
 dic={0:(0,0),1:(0,1),2:(0,2),3:(1,0),4:(1,1),5:(1,2),6:(2,0),7:(2,1),8:(2,2)}
 #Dictionnaire qui renvoie le numéro de la grille
